f4c6b95ffaa7b8609222a87ffbc1be0c455c7f28
David Müller <d.mueller@workmatrix.de>
2016-10-13 20:18:27 +0200

    added bitbucket-pipelines.yml and phpunit bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+// load test files
+require_once __DIR__.'/KungFoo/Controllers/Common/ControllerWithAnnotations.php';
+require_once __DIR__.'/KungFoo/Controllers/Common/ControllerWithAnnotations2.php';
+require_once __DIR__.'/KungFoo/Controllers/Common/ControllerWithAnnotationsDispatch.php';
+require_once __DIR__.'/KungFoo/Controllers/Common/ControllerWithAnnotationsErrors.php';
